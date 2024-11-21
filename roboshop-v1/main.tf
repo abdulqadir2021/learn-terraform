b3887da0eefaac193c7e67d12a122868fcb27d3d
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-22 05:22:57 +0530

    Install config changed to shell

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -7,3 +7,92 @@ resource "aws_instance" "frontend" {
     }
 }
 
+resource "aws_instance" "mongodb" {
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_ids = ["sg-0880ecf773230947b"]
+    tags = {
+        Name = "mongodb"
+    }
+}
+
+resource "aws_instance" "Catalogue" {
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_ids = ["sg-0880ecf773230947b"]
+    tags = {
+        Name = "Catalogue"
+    }
+}
+
+resource "aws_instance" "redis" {
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_ids = ["sg-0880ecf773230947b"]
+    tags = {
+        Name = "redis"
+    }
+}
+
+resource "aws_instance" "user" {
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_ids = ["sg-0880ecf773230947b"]
+    tags = {
+        Name = "user"
+    }
+}
+
+resource "aws_instance" "Cart" {
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_ids = ["sg-0880ecf773230947b"]
+    tags = {
+        Name = "Cart"
+    }
+}
+
+resource "aws_instance" "mysql" {
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_ids = ["sg-0880ecf773230947b"]
+    tags = {
+        Name = "mysql"
+    }
+}
+
+resource "aws_instance" "shipping" {
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_ids = ["sg-0880ecf773230947b"]
+    tags = {
+        Name = "shipping"
+    }
+}
+
+resource "aws_instance" "rabbitmq" {
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_ids = ["sg-0880ecf773230947b"]
+    tags = {
+        Name = "rabbitmq"
+    }
+}
+
+resource "aws_instance" "payment" {
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_ids = ["sg-0880ecf773230947b"]
+    tags = {
+        Name = "payment"
+    }
+}
+
+resource "aws_instance" "dispatch" {
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_ids = ["sg-0880ecf773230947b"]
+    tags = {
+        Name = "dispatch"
+    }
+}
