f15bf22bdd45649c8a3000f7bbfe8ee18b6b9782
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-22 05:54:52 +0530

    Install config changed to shell

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -7,6 +7,14 @@ resource "aws_instance" "frontend" {
     }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z09687201RU9RU0QEJJY1"
+  name    = "frontend-dev.abdulqadir.shop"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
     ami           = "ami-09c813fb71547fc4f"
     instance_type = "t3.small"
