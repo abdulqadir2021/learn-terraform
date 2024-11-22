5718b084e2dfa27763906aff5297978e50ca110e
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-23 02:05:40 +0530

    Install config changed to shell

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -12,3 +12,55 @@ output "URL1" {
     value = "URL - ${var.URL}"
 }
 
+# Data types
+variable "num1" {
+    default = 10
+}
+
+variable "bool1" {
+    default = true
+}
+
+variable "strl" {
+    default = "Hello"
+}
+
+# Numbers, Booleans, & Strings
+
+# Variable types
+variable "str2" {
+    default = "World"
+}
+
+variable "list1" {
+    default = [
+        10,
+        20,
+        "xyz",
+        false
+    ]
+}
+
+# List can have a combination of multiple data types also.
+variable "map1" {
+  default = {
+      aws = {
+              trainer = "john"
+              duration = 30
+      }
+      devops = {
+              trainer = "Steve"
+              duration = 20
+      }
+  }
+}
+
+output "list1_1" {
+    value = var.list1[2]
+}
+
+output "map1_aws_trainer" {
+    value = var.map1["aws"]["trainer"]
+}
+
+
