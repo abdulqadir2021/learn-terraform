67392e78a2a05096f3f4e64adc5055a668789b10
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-23 01:47:17 +0530

    Install config changed to shell

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -7,7 +7,8 @@ output "URL" {
     value = var.URL
 }
 
-# variable is a combination of some other string. Then we have to use ${}
+# Variable is a combination of some other string. Then we have to use ${}
 output "URL1" {
     value = "URL - ${var.URL}"
 }
+
