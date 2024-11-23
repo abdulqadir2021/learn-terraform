ab0af3361422a38fed3e25f2b0cb51a4aa1a6e27
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-23 21:51:12 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ 04-data/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0880ecf773230947b"]
+  tags = {
+    Name = "test"
+  }
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "RHEL-9-DevOps-Practice"
+  owners = ["973714476881"]
+}
