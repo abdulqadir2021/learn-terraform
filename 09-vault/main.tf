0bef426dc0b2c31e0cc452f566bbbfe52950a455
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-29 18:29:37 +0530

    Merge remote-tracking branch 'origin/main'

--- 09-vault/main.tf
+++ 09-vault/main.tf
@@ -1,25 +1,18 @@
 provider "vault" {
-  address = "https://172.31.36.180:8200"
+  address = "https://172.31.16.175:8200"
   token   = var.vault_token
   skip_tls_verify = true
 }
 
 variable "vault_token" {}
 
-data "vault_generic_secret" "example" {
-  path = "test/my_credentials"
-}
+data "vault_kv_secret_v2" "example" {
+  name  = "my_credentials"
+  mount = "test"
 
-# data "vault_kv_secret_v2" "example" {
-#   name  = "my_credentials"
-#   mount = "test"
-# }
+}
 
 resource "local_file" "foo" {
-  content  = data.vault_generic_secret.example.data["password"]
+  content  = "data.vault_kv_secret.v2.example."
   filename = "/tmp/secret"
 }
-
-# output "json" {
-#   value = data.vault_kv_secret_v2.example.data_json
-# }
