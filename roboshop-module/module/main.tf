ece72007cea26ab7681d72f41dfca5f8f17d8e04
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2025-02-03 14:57:42 +0530

    Merge remote-tracking branch 'origin/main'

--- roboshop-module/module/main.tf
+++ roboshop-module/module/main.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "instance" {
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-0880ecf773230947b"]
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-088e5b9464686ff19"]
   tags = {
     Name = var.component_name
   }
 }
 
 resource "aws_route53_record" "record" {
-  zone_id = "Z09687201RU9RU0QEJJY1"
+  zone_id = "Z00064743L2THMADA4LWZ"
   name    = "${var.component_name}-dev.abdulqadir.shop"
   type    = "A"
   ttl     = "30"
