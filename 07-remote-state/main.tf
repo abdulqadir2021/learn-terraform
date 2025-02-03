1762df789fdedd177edd056bce008bbb0690dd1c
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2025-02-03 16:10:15 +0530

    Merge remote-tracking branch 'origin/main'

--- 07-remote-state/main.tf
+++ 07-remote-state/main.tf
@@ -1,10 +1,11 @@
 terraform {
   backend "s3" {
-    bucket = "terraform.d81"
+    bucket = "terraform.d81aq"
     key    = "sample/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 resource "null_resource" "test" {}
+
 
