23aba71638dc1c46fd5d521d7bd9a2528393b348
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2025-07-13 02:49:53 +0530

    project roboshop

--- roboshop-count-loop/main.tf
+++ roboshop-count-loop/main.tf
@@ -16,7 +16,7 @@ variable "instances" {
 resource "aws_instance" "instance" {
      count = length(var.instances)
     ami           = "ami-09c813fb71547fc4f"
-    instance_type = "t3.small"
+    instance_type = "t2.micro"
     vpc_security_group_ids = ["sg-0246835708d5d8354"]
     tags = {
         Name = var.instances[count.index]
