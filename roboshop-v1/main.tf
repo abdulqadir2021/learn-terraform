3ecf4aadfd7a64fb81023c0169ceb15fb3e751db
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2026-09-03 19:49:37 +0530

    project roboshop 26

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "frontend" {
-    ami           = "ami-09c813fb71547fc4f"
+    ami           = "ami-0220d79f3f480ecf5"
     instance_type = "t2.micro"
 
 #     vpc_security_group_ids = ["sg-04b42790bbde7cf01"]
