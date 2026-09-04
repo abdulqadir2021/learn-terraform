c81df6613a8f8d0161a9abfbf53c2437953c6f4f
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2026-09-04 18:04:21 +0530

    project roboshop 26

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -2,10 +2,10 @@ variable "URL" {
     default = "example.com"
 }
 
-# ## Access variable
-# output "URL" {
-#     value = var.URL
-# }
+## Access variable
+output "URL" {
+    value = var.URL
+}
 # # Variable is a combination of some other string. Then we have to use ${}
 # output "URL1" {
 #     value = "URL - ${var.URL}"
