4c6c093b1731230247ff3e038eb697b38dac2286
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2026-09-06 02:06:41 +0530

    project roboshop 26

--- 08-multi-env-code-structure/modules/ec2/main.tf
+++ 08-multi-env-code-structure/modules/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instance" {
   ami           = "ami-0220d79f3f480ecf5"
-  instance_type = "t3.small"
+  instance_type = "te.micro"
   vpc_security_group_ids = ["sg-04b42790bbde7cf01"]
   tags = {
     Name = "test.${var.env}"
