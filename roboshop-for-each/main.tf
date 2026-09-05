0b94bc3b76681645369367e791d35ab3dc62c874
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2026-09-05 20:16:31 +0530

    project roboshop 26

--- roboshop-for-each/main.tf
+++ roboshop-for-each/main.tf
@@ -15,7 +15,7 @@ variable "instances" {
 }
 
 resource "aws_instance" "instance" {
-     for_each = var.instances
+    for_each = var.instances
     ami           = "ami-0220d79f3f480ecf5"
     instance_type = "t3.micro"
     vpc_security_group_ids = ["sg-04b42790bbde7cf01"]
@@ -25,13 +25,14 @@ resource "aws_instance" "instance" {
 }
 
 resource "aws_route53_record" "record" {
-  count = length(var.instances)
+  for_each = var.instances
   zone_id = "Z0272961EHM02ZV0QE8D"
   name    = "${each.key}-dev.yeloabhi.fun"
   type    = "A"
   ttl     = "30"
   records = [aws_instance.instance[each.key].private_ip]
 }
+
 
 
 
