0d92ebce4390e1b4e5bf61b791e3dea0c86bf1ff
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2026-09-09 18:50:35 +0530

    project roboshop 26

--- 09-vault/main.tf
+++ 09-vault/main.tf
@@ -16,10 +16,10 @@ data "vault_kv_secret_v2" "example" {
 
 }
 
-# resource "local_file" "foo" {
-#   content  = data.vault_generic_secret.example.data["password"]
-#   filename = "/tmp/secret"
-# }
+resource "local_file" "foo" {
+  content  = data.vault_generic_secret.example.data["password"]
+  filename = "/tmp/secret"
+}
 
 # output "json" {
 #  value = data.vault_kv_secret_v2.example.data_json
